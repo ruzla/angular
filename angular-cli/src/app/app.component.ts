9f75e8adccd5a33712858cff6cd15558e1ce1eb0
Russell <russelldoe@hotmail.co.uk>
2020-02-29 23:25:50 +0000

    added library

--- angular-cli/src/app/app.component.ts
+++ angular-cli/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { LoggerService } from 'my-lib';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-cli';
+
+  constructor(logger: LoggerService) {
+    logger.log('hello angular');
+  }
 }
