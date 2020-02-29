b8cb1da2227ce9a51bf96b11b96a5d68ffd6a0b5
Russell <russelldoe@hotmail.co.uk>
2020-02-29 21:35:09 +0000

    angular cli update

--- angular-cli/src/app/admin/admin.component.ts
+++ angular-cli/src/app/admin/admin.component.ts
@@ -6,10 +6,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  users: any[];
+  hasPermission = true;
 
   constructor() { }
 
   ngOnInit() {
+    if (this.hasPermission) {
+      this.getUsers()
+       .then(users => this.users = users)
+       .catch(e => console.log(e.message));
+    } else {
+      this.users = [];
+    }
+  }
+
+  async getUsers() {
+    return [
+      {name: 'John', email: 'john@angular.com'},
+      {name: 'Russ', email: 'russ@angular.com'},
+    ]
   }
 
 }
--- angular-cli/src/app/app.component.spec.ts
+++ angular-cli/src/app/app.component.spec.ts
@@ -1,15 +1,42 @@
+import { NavComponent } from './nav/nav.component';
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        LayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatMenuModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, NavComponent
       ],
     }).compileComponents();
   }));
@@ -26,10 +53,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('angular-cli');
   });
 
-  it('should render title', () => {
+  it('should render app-nav in a div tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-cli app is running!');
+    expect(compiled.querySelector('div > app-nav')).toBeDefined('angular-cli app is running!');
   });
 });
