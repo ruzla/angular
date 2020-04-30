e87f49db7a57b9d91c6181a305f28c42b1090257
Russell <russelldoe@hotmail.co.uk>
2020-04-30 15:29:54 +0100

    child routes

--- angular-routing/src/app/products/product.module.ts
+++ angular-routing/src/app/products/product.module.ts
@@ -2,11 +2,13 @@ import { ProductResolver } from './product-resolver.service';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
 
 @NgModule({
   imports: [
@@ -18,13 +20,31 @@ import { SharedModule } from '../shared/shared.module';
         resolve: { resolvedData: ProductResolver} },
       { path: 'products/:id/edit',
         component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver} }
+        resolve: { resolvedData: ProductResolver},
+        children: [
+          {
+            path: '',
+            redirectTo: 'info',
+            pathMatch: 'full'
+          },
+          {
+            path: 'info',
+            component: ProductEditInfoComponent
+          },
+          {
+            path: 'tags',
+            component: ProductEditTagsComponent
+          }
+        ]
+      }
     ])
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
-    ProductEditComponent
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
   ]
 })
 export class ProductModule { }
