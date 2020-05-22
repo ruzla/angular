8a245ea7cc07f9d2668d6bc3442b70364e9a5bab
Russell <russelldoe@hotmail.co.uk>
2020-05-22 23:07:20 +0100

    added ngrx

---
+++ angular-ngrx/src/app/products/state/product.reducer.ts
@@ -0,0 +1,34 @@
+import { Product } from './../product';
+import * as fromRoot from '../../state/app.state';
+
+interface State extends fromRoot.State {
+    products: ProductState;
+}
+
+export interface ProductState {
+    showProductCode: boolean;
+    currentProduct: Product;
+    products: Product[];
+}
+
+const initialState: ProductState = {
+    showProductCode: true,
+    currentProduct: null,
+    products: []
+};
+
+export function reducer(state = initialState, action): ProductState {
+    switch (action.type) {
+
+        case 'TOGGLE_PRODUCT_CODE':
+            // console.log('existing state: ' + JSON.stringify(state));
+            // console.log('payload: ' + action.payload);
+            return {
+                ...state,
+                showProductCode: action.payload
+            };
+
+        default:
+            return state;
+    }
+}
